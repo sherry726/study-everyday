a4056a933d8cba46f4bd605ddb0931a5772fb850
王琴 <wqbistu@163.com>
2022-10-01 10:02:32 +0800

    知识点补充

---
+++ 待学习.js
@@ -0,0 +1,16 @@
+/*
+1、Vue项目中，package.json里面的browserslist配置项
+  "browserslist": [
+    "> 1%",
+    "last 7 versions",
+    "not ie <= 8",
+    "last 10 Chrome versions",
+    "last 5 Firefox versions",
+    "Safari >= 6"
+  ]
+
+
+
+
+
+*/
