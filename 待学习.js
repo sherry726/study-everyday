59309af53c11b651d55752d5d7f827e21c7a3f85
王琴 <wqbistu@163.com>
2023-01-09 22:02:00 +0800

    知识点记录

--- 待学习.js
+++ 待学习.js
@@ -14,3 +14,36 @@
 
 
 */
+function red() {
+  console.log('red');
+}
+
+function green() {
+  console.log('green');
+}
+
+function yellow() {
+  console.log('yellow');
+}
+
+var light = function (timer, cb) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      cb();
+      resolve();
+    }, timer);
+  });
+};
+
+var step = function () {
+  Promise.resolve().then(function () {
+    return light(3000, red);
+  }).then(function () {
+    return light(2000, green);
+  }).then(function () {
+    return light(1000, yellow);
+  }).then(function () {
+    step();
+  });
+}
+step();
